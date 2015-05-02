efea8492d2cfb0f000e5b27d09d0356fb22c803c
kieferbonk <sartanen@gmail.com>
2015-05-03 00:59:32 +0300

    basic styles, combine lists, mutate data as necessary

--- src/common/services/feedService.js
+++ src/common/services/feedService.js
@@ -2,7 +2,7 @@
 
 angular.module('newzak.feedServiceModule', [])
 
-.factory('feedService', function ($http, conf, lodash) {
+.factory('feedService', function ($http, $q, conf, lodash) {
 
 	var fetchFeed = function (url) {
 		return $http.jsonp('http://ajax.googleapis.com/ajax/services/feed/load?v=1.0&num=100&q=' + url + 
@@ -18,13 +18,54 @@ angular.module('newzak.feedServiceModule', [])
 		return lists;
 	};
 
-	var getMasterList = function () {
-		var list = [];
+	var brandSource = function (source) {
+		var result;
 
-		lodash.forEach(fetchAllLists(), function (n) {
-			n.then(function (feed) {
-				if (200 === feed.data.responseStatus) { list.push.apply(list, feed.data.responseData.feed.entries); }
+		switch (source) {
+			case 'Drowned In Sound // Feed':
+				result = 'dis';
+				break;
+			case 'Album Reviews - Pitchfork':
+				result = 'pitchfork';
+				break;
+		}
+
+		return result;
+	};
+
+	var generateTimestamp = function (dateStr) {
+		var dateObj = new Date(dateStr);
+
+		return dateObj.getTime();
+	};
+
+	var combineLists = function (lists) {
+		var entries = [],
+			type;
+
+		lodash.forEach(lists, function (list) {
+			if (200 !== list.data.responseStatus) { return; }
+
+			lodash.forEach(list.data.responseData.feed.entries, function (entry) {
+				entry.type = brandSource(list.data.responseData.feed.title);
+				entries.push(entry);
 			})
+		});
+
+		return entries;
+	};
+
+	var getMasterList = function () {
+		var list = [],
+			entries;
+
+		$q.all(fetchAllLists()).then(function (lists) {
+			entries = combineLists(lists);
+
+			lodash.forEach(entries, function (entry) {
+				entry.timestamp = generateTimestamp(entry.publishedDate);
+				list.push(entry);
+			});
 		});
 
 		return list;
